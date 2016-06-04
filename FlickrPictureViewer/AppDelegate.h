bad92e1271200d3f623eaab1f82b28cbe50626b7
自分 <Hiromitsu.Oka@jp.sony.com>
2016-06-04 23:04:33 +0900

    Initial Commit

---
+++ FlickrPictureViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrPictureViewer
+//
+//  Created by 岡宏充 on 2016/06/04.
+//  Copyright © 2016年 Hiromitsu Oka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrPictureViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrPictureViewer
+//
+//  Created by 岡宏充 on 2016/06/04.
+//  Copyright © 2016年 Hiromitsu Oka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
